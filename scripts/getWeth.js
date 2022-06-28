abc0d15840649734053c7dad7a002d675b7ca8fe
amatureApe <amatureApe@gmail.com>
2022-06-28 10:51:19 -0500

    add IWeth interface

--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -2,6 +2,8 @@ const { getNamedAccounts } = require("hardhat");
 
 async function getWeth() {
   const { deployer } = await getNamedAccounts();
+
+  const iWeth = await ethers.getContractAt("IWeth", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", deployer);
 }
 
 module.exports = { getWeth }
